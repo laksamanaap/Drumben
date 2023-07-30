fc5f6ff1df5d0956b62796facea5b9b3d44a3bf3
laksamanaap <laksamana.arya1412@gmail.com>
2023-07-30 23:26:54 +0700

    feat : drum

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var x = document.querySelectorAll(".drum");
 for (var i = 0; i < x.length; i++) {
     document.querySelectorAll(".drum")[i].addEventListener("click", function () {
         this.style.color = "white"; // this access the curent object 
-        // var audio = new Audio("sounds/crash.mp3");
-        // audio.play(); 
+        var audio = new Audio("sounds/crash.mp3");
+        audio.play();
     });
 }
