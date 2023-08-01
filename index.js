617ec923776a8cc0f017e33bd5635885b56aadad
laksamanaap <laksamana.arya1412@gmail.com>
2023-08-01 21:09:54 +0700

    change style

--- index.js
+++ index.js
@@ -10,55 +10,58 @@ let drumActive = document.querySelectorAll(".drum"); // => Get drum
 //     });
 // }
 
-for (var i = 0; i < drum.length; i++) {
-    document.querySelectorAll(".drum")[i].addEventListener("click", function () { // => Get all drum
-        var toggle = this.innerHTML;
-        // setSound(toggle); 
+for (let i = 0; i < drum.length; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", function (click) {
+        var press = this.innerHTML;
+        setSound(press);
     });
-}
+} // => Click function
 
-document.addEventListener("keydown", function (e) { // => Keyboard Pressed
-    setSound(e.key);
-});
+for (var i = 0; i < drum.length; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("keydown", function (press) { // => Get all drum
+        setSound(press.key);
+    });
+} // => Pressed Function
 
 function setSound(key) {
     switch (key) { // Specify the drum using swith
         case "w":
             var w = new Audio("sounds/tom-1.mp3")
             w.play();
-            // console.log(key);
+            console.log(key);
             break;
         case "a":
             var a = new Audio("sounds/tom-2.mp3")
             a.play();
-            // console.log(key);
+            console.log(key);
             break;
         case "s":
             var s = new Audio("sounds/tom-3.mp3")
             s.play();
-            // console.log(key);
+            console.log(key);
             break;
         case "d":
             var d = new Audio("sounds/tom-4.mp3")
             d.play();
-            // console.log(key);
+            console.log(key);
             break;
         case "j":
             var j = new Audio("sounds/snare.mp3")
             j.play();
-            // console.log(key);
+            console.log(key);
             break;
         case "k":
             var k = new Audio("sounds/crash.mp3")
             k.play();
-            // console.log(key);
+            console.log(key);
             break;
         case "l":
             var l = new Audio("sounds/kick-bass.mp3")
             l.play();
-            // console.log(key);
+            console.log(key);
             break;
 
         default:
     }
 }
+
